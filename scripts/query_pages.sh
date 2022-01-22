ede75ae53b1f21177fd37a47477b75498bd05d2f
Sergio Sánchez Zavala <chekos.data@gmail.com>
2022-01-21 22:47:47 -0800

    Update query_pages.sh

--- scripts/query_pages.sh
+++ scripts/query_pages.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
-curl -X POST 'https://api.notion.com/v1/search' \
-  -H 'Authorization: Bearer '"$NOTION_TOKEN"''
-  -H 'Content-Type: application/json' \
-  -H "Notion-Version: 2022-01-20" \
-  --data '{
-    "sort":{
-      "direction":"ascending",
-      "timestamp":"last_edited_time"
-    }
-  }'
+
+curl --request POST \
+     --url https://api.notion.com/v1/search \
+     --header 'Accept: application/json' \
+     --header 'Authorization: Bearer '"$NOTION_TOKEN"'' \
+     --header 'Content-Type: application/json' \
+     --header 'Notion-Version: 2022-01-20' \
+     --data '{"page_size":100}'
