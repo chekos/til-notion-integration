113579f6ef1b317b768180bdc2f9cb27dd09cfae
Sergio Sánchez Zavala <chekos.data@gmail.com>
2022-01-25 23:21:17 +0000

    fix error when outputing objects to json

--- scripts/query_til_db.sh
+++ scripts/query_til_db.sh
@@ -7,5 +7,5 @@ curl --request POST \
 --header 'Content-Type: application/json' \
 --header 'Notion-Version: 2021-08-16' \
 --data '{"page_size":100}' \
-| jq '.results[] | {id: .id, title: .properties.Name.title[0].text.content, tags: [.properties.Tags.multi_select[].name], created: .properties.Created.created_time } | join(",")' \
+| jq '[.results[] | {id: .id, title: .properties.Name.title[0].text.content, tags: [.properties.Tags.multi_select[].name], created: .properties.Created.created_time }]' \
 > tils.json
