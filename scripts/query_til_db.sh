73a5e930b79cfc805908a0ce666ac0fcb8f2fb10
Sergio Sánchez Zavala <chekos.data@gmail.com>
2022-01-25 22:53:10 +0000

    move jq to script

--- scripts/query_til_db.sh
+++ scripts/query_til_db.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
 curl --request POST \
-     --url https://api.notion.com/v1/databases/$TIL_DATABASE_ID/query \
-     --header 'Accept: application/json' \
-     --header 'Authorization: Bearer '"$NOTION_TOKEN"'' \
-     --header 'Content-Type: application/json' \
-     --header 'Notion-Version: 2021-08-16' \
-     --data '{"page_size":100}'
+--url https://api.notion.com/v1/databases/$TIL_DATABASE_ID/query \
+--header 'Accept: application/json' \
+--header 'Authorization: Bearer '"$NOTION_TOKEN"'' \
+--header 'Content-Type: application/json' \
+--header 'Notion-Version: 2021-08-16' \
+--data '{"page_size":100}' \
+| jq '.results[] | {id: .id, title: .properties.Name.title[0].text.content, tags: [.properties.Tags.multi_select[].name], created: .properties.Created.created_time } | join(",")' \
+> tils.json
