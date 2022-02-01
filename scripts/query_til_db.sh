c054c67a1757d959c4830e7e5cd747e2c49cf094
Sergio Sánchez Zavala <chekos.data@gmail.com>
2022-02-01 15:41:27 +0000

    filter tils

--- scripts/query_til_db.sh
+++ scripts/query_til_db.sh
@@ -6,6 +6,22 @@ curl --request POST \
 --header 'Authorization: Bearer '"$NOTION_TOKEN"'' \
 --header 'Content-Type: application/json' \
 --header 'Notion-Version: 2021-08-16' \
---data '{"page_size":100}' \
+--data '{"page_size":100, "filter": {
+  "and": [
+    {
+      "property": "Draft",
+      "checkbox": {
+        "equals": false
+      }
+    },
+    {
+      "property": "Archive",
+      "checkbox": {
+        "equals": false
+      }
+    }
+  ]
+}
+}' \
 | jq '[.results[] | {id: .id, title: .properties.Name.title[0].text.content, tags: [.properties.Tags.multi_select[].name], created: .properties.Created.created_time }]' \
 > tils.json
